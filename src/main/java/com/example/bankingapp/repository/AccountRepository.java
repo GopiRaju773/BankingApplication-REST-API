4e563b22a4e3ccf0a62cc19b6df6dc66383f48fe
GopiRaju773 <orchugopiraju262@gmail.com>
2025-08-28 22:54:31 +0530

    Update AccountRepository.java

--- src/main/java/com/example/bankingapp/repository/AccountRepository.java
+++ src/main/java/com/example/bankingapp/repository/AccountRepository.java
@@ -1,4 +1,7 @@
 package com.example.bankingapp.repository;
 
-public interface AccountRepository {
+import com.example.bankingapp.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
