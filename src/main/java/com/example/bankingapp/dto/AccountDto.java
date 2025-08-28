ce019b1f1589422f7e74752d4f67457ccbbb13cb
GopiRaju773 <orchugopiraju262@gmail.com>
2025-08-28 22:51:59 +0530

    Update AccountDto.java

--- src/main/java/com/example/bankingapp/dto/AccountDto.java
+++ src/main/java/com/example/bankingapp/dto/AccountDto.java
@@ -1,4 +1,15 @@
 package com.example.bankingapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AccountDto {
+    private Long  id;
+    private final String accountHolderName;
+    private Double balance;
+    private Long accountNumber;
+
+
 }
