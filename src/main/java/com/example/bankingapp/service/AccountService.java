5269b8790f671e99680b013750d9c3b8214c3b1e
GopiRaju773 <orchugopiraju262@gmail.com>
2025-08-28 22:56:21 +0530

    Update AccountService.java

--- src/main/java/com/example/bankingapp/service/AccountService.java
+++ src/main/java/com/example/bankingapp/service/AccountService.java
@@ -1,4 +1,14 @@
 package com.example.bankingapp.service;
 
+import com.example.bankingapp.dto.AccountDto;
+
+import java.util.List;
+
 public interface AccountService {
+    AccountDto createAccount(AccountDto accountDto);
+    AccountDto  getAccountById(Long id);
+    AccountDto deposit(Long id, Double amount);
+    AccountDto withdraw(Long id, Double amount);
+    List<AccountDto> getAllAccounts();
+    void deleteAccount(Long id);
 }
