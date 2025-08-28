ae0816a0c8e303e7514e5c2d9a805970bb2a27b7
GopiRaju773 <orchugopiraju262@gmail.com>
2025-08-28 22:53:47 +0530

    Update AccountMapper.java

--- src/main/java/com/example/bankingapp/mapper/AccountMapper.java
+++ src/main/java/com/example/bankingapp/mapper/AccountMapper.java
@@ -1,4 +1,25 @@
 package com.example.bankingapp.mapper;
 
+import com.example.bankingapp.dto.AccountDto;
+import com.example.bankingapp.entity.Account;
+
 public class AccountMapper {
+    public static Account mapToAccount(AccountDto accountDto){
+        Account account =new Account(
+                accountDto.getId(),
+                accountDto.getAccountHolderName(),
+                accountDto.getBalance(),
+                accountDto.getAccountNumber()
+        );
+        return account;
+    }
+public static AccountDto mapToAccountDto(Account account){
+        AccountDto AccountDto = new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance(),
+                account.getAccountNumber()
+        );
+        return AccountDto;
+}
 }
